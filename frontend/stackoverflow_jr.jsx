2d7d223eac82dbefefb15422eee665b37fda30f7
bcwan <bcwan8@gmail.com>
2020-04-28 12:07:27 -0700

    Create store for app

--- frontend/stackoverflow_jr.jsx
+++ frontend/stackoverflow_jr.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//import { login, logout, signup } from './util/session_api_util'
+import configureStore from './store/store';
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  // window.login = login;
-  // window.logout = logout;
-  // window.signup = signup;
+  const store = configureStore();
+
+   // test out store
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   ReactDOM.render(<h1>Welcome to StackOverflow JR</h1>, root);
 });
+
+
+// api util testing
+// =================
+// import { login, logout, signup } from './util/session_api_util'
+// window.login = login;
+// window.logout = logout;
+// window.signup = signup;
