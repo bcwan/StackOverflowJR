bfeb2ceebb83bec1592b07e5e771bb1c576cea13
bcwan <bcwan8@gmail.com>
2020-04-28 15:09:03 -0700

    Render root component on entry file

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>StackOverflow Jr</h1>
+  </div>
+);
+
+export default App;
--- frontend/stackoverflow_jr.jsx
+++ frontend/stackoverflow_jr.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import configureStore from './store/store';
 
+import Root from './components/root';
+
+
 // test out thunk actions
-import { login, logout, signup } from './actions/session_actions'
+// import { login, logout, signup } from './actions/session_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore();
 
   // test out store
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup;
+  // window.login = login;
+  // window.logout = logout;
+  // window.signup = signup;
 
-  ReactDOM.render(<h1>Welcome to StackOverflow JR</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
 
 
