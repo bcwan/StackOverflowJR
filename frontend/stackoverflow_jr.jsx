632c137848c48b8c3c285fabd94267b168f72ce8
bcwan <bcwan8@gmail.com>
2020-04-28 00:27:54 -0700

    finalized user auth code for rails including ajax

--- frontend/stackoverflow_jr.jsx
+++ frontend/stackoverflow_jr.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//import { login, logout, signup } from './util/session_api_util'
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  // window.login = login;
+  // window.logout = logout;
+  // window.signup = signup;
+
   ReactDOM.render(<h1>Welcome to StackOverflow JR</h1>, root);
 });
