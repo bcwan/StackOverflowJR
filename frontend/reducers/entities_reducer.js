85ab120cbd84afd580ee8ab47e5e4f5203348fed
bcwan <bcwan8@gmail.com>
2020-05-02 23:36:42 -0700

    Update entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import QuestionReducer from './questions_reducer';
+import AnswerReducer from './answers_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
-  questions: QuestionReducer
+  questions: QuestionReducer,
+  answers: AnswerReducer
 });
 
 export default entitiesReducer;
