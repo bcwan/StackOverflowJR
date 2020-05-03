6aa45fc326d585f7efaf58f5d3167431520f7d06
bcwan <bcwan8@gmail.com>
2020-05-02 22:08:36 -0700

    Create answers actions

---
+++ frontend/actions/answers_actions.js
@@ -0,0 +1,6 @@
+import * as AnswerApiUtil from '../util/answers_api_util';
+
+export const RECEIVE_QUESTION_ANSWERS = 'RECEIVE_QUESTION_ANSWERS';
+export const RECEIVE_ANSWER = 'RECEIVE_ANSWER';
+export const REMOVE_ANSWER = 'REMOVE_ANSWER';
+
