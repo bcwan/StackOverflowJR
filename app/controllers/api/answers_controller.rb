86f3e64b34b6310f97773ae2f5bc8e05a73ca320
bcwan <bcwan8@gmail.com>
2020-05-30 22:06:18 -0700

    Remove redundant comments

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -1,8 +1,5 @@
 class Api::AnswersController < ApplicationController
 
-  # we want user to be logged in in order to manage his/her own answers
-  # before_action :require_logged_in, only: [:update, :destroy, :create]
-
   def index
     @answers = Answer.where(question_id: params[:question_id])
     render :index
