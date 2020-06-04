f27afd30b4e455631681a1d0f23669392c92423a
bcwan <bcwan8@gmail.com>
2020-06-03 18:51:43 -0700

    Create table for question votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id          :bigint           not null, primary key
+#  vote_type   :integer          default(0), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  question_id :integer          not null
+#  user_id     :integer          not null
+#
 class Vote < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_012731) do
+ActiveRecord::Schema.define(version: 2020_06_04_013633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2020_06_04_012731) do
     t.string "username", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "question_id", null: false
+    t.integer "user_id", null: false
+    t.integer "vote_type", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
