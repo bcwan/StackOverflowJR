0c44456615d5604e9f9d66d69058f11f57a76ded
bcwan <bcwan8@gmail.com>
2020-06-05 15:02:09 -0700

    Update upvotes create method

--- app/controllers/api/upvotes_controller.rb
+++ app/controllers/api/upvotes_controller.rb
@@ -1,6 +1,15 @@
 class Api::UpvotesController < ApplicationController
 
+  # if user hasn't done an upvote, create upvote
   def create
+    @upvote = Upvote.new
+    @upvote.user_id = current_user.id
+    @upvote.question_id = params[:question_id]
+    if @upvote.save
+      render 'api/questions/show'
+    else
+      render json: ['Cannot be upvoted'], status: 422
+    end
 
   end
 
