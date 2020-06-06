def5efe79697c415086f6ca48cdd7aa0bc1e80a6
bcwan <bcwan8@gmail.com>
2020-06-05 23:56:21 -0700

    Create ajax calls for downvoting

---
+++ frontend/util/downvote_api_util.js
@@ -0,0 +1,13 @@
+export const createDownvote = (questionId) => (
+  $.ajax({
+    method: 'POST',
+    url: `/api/questions/${questionId}/downvotes`
+  })
+);
+
+export const deleteDownvote = (questionId) => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/questions/${questionId}/downvotes/`
+  })
+);
