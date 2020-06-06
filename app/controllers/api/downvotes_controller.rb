f3f94bf9a65000553164fe34ab33165daa1d0244
bcwan <bcwan8@gmail.com>
2020-06-05 21:49:16 -0700

    Add downvotes create method in controller

--- app/controllers/api/downvotes_controller.rb
+++ app/controllers/api/downvotes_controller.rb
@@ -1,6 +1,23 @@
 class Api::DownvotesController < ApplicationController
 
+  # if user hasn't done an downvote, create downvote
+  # also return a boolean if user downvoted quesiton in show page
   def create
+    if already_voted?
+      render json: ['User has an downvote already'], status: 422
+    else
+      @downvote = Downvote.new
+      # @downvote.user_id = current_user.id
+      @downvote.user_id = 2
+      @downvote.question_id = params[:question_id]
+      byebug
+      if @downvote.save
+        @question = @downvote.question
+        render 'api/questions/show'
+      else
+        render json: ['Cannot downvote'], status: 422
+      end
+    end
 
   end
 
@@ -10,7 +27,7 @@ class Api::DownvotesController < ApplicationController
   
   private
     def already_voted?
-    # Upvote.where(user_id: current_user.id, question_id: params[:question_id]).exists?
+    # Downvote.where(user_id: current_user.id, question_id: params[:question_id]).exists?
       Downvote.where(user_id: 2, question_id: params[:question_id]).exists?
     end
 
