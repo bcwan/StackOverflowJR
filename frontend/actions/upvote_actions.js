c9f5ef9c73bcc8aa0edce9d25efa78ffccfab530
bcwan <bcwan8@gmail.com>
2020-06-06 01:09:13 -0700

    Create actions for upvote

--- frontend/actions/upvote_actions.js
+++ frontend/actions/upvote_actions.js
@@ -3,3 +3,13 @@ import * as UpvoteApiUtil from '../util/upvote_api_util';
 export const RECEIVE_UPVOTE = 'RECEIVE_UPVOTE';
 export const REMOVE_UPVOTE = 'REMOVE_UPVOTE';
 
+const receiveUpvote = questionId => ({
+  type: RECEIVE_UPVOTE,
+  questionId
+});
+
+const removeUpvote = questionId => ({
+  type: REMOVE_UPVOTE,
+  questionId
+});
+
