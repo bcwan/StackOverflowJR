7b2c3ab3ce101ff15c34e92782111ca096c8e8f7
bcwan <bcwan8@gmail.com>
2020-06-06 01:23:57 -0700

    Create thunk actions for upvote

--- frontend/actions/upvote_actions.js
+++ frontend/actions/upvote_actions.js
@@ -13,3 +13,19 @@ const removeUpvote = questionId => ({
   questionId
 });
 
+// thunk action creators
+// the response from upvoting returns question attributes with the
+// upvotes number updated ~ similar to fetchQuestion thunk action in questions_actions.js
+// it's how the controllers were set
+export const createUpvote = (questionId) => (dispatch) => {
+  return UpvoteApiUtil.createUpvote(questionId)
+    .then(updatedUpvotes => dispatch(receiveUpvote(updatedUpvotes)))
+};
+
+// delete will returns an updated upvote number if user decides not to upvote
+// it returns all the question attributes, including the new upvote number ~ how controllers are set
+export const deleteUpvote = (questionId) => (dispatch) => {
+  return UpvoteApiUtil.deleteUpvote(questionId)
+    .then(updatedUpvotes => dispatch(removeUpvote(updatedUpvotes)))
+};
+
