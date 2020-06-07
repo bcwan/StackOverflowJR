0052706e7b8255c3e50f221514f34879285fe67d
bcwan <bcwan8@gmail.com>
2020-06-06 18:45:36 -0700

    Set up mSTP and mDTP for votes

--- frontend/components/votes/question_votes_container.js
+++ frontend/components/votes/question_votes_container.js
@@ -1,4 +1,17 @@
 import { connect } from 'react-redux';
 import Votes from './votes';
-import { } from '../../actions/upvote_actions';
-import { } from '../../actions/downvote_actions';
+import { createUpvote, deleteUpvote } from '../../actions/upvote_actions';
+import { createDownvote, deleteDownvote } from '../../actions/downvote_actions';
+
+const mSTP = (state) => {
+  return {
+
+  }
+};
+
+const mDTP = (dispatch) => {
+  return {
+    createUpvote: (questionId) => dispatch(createUpvote(questionId)),
+    deleteUpvote: (questionId) => dispatch(deleteUpvote(questionId)),
+  }
+};
