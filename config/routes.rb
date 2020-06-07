33ce0ad0ccfd5b5fbc157bab22ca657e1f20e171
bcwan <bcwan8@gmail.com>
2020-06-07 16:37:42 -0700

    Remove comments in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
 
-  # user#create - signup
-  # session#create - login
-  # session#destroy - logout
-
   namespace :api, defaults: { format: :json } do
 
     resources :users, only: [:create]
