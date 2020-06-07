2e9a2f2bdb146c20350c1b07340c56a979cf9650
bcwan <bcwan8@gmail.com>
2020-06-06 20:45:37 -0700

    Add props to question votes container

--- frontend/components/votes/question_votes_container.js
+++ frontend/components/votes/question_votes_container.js
@@ -3,9 +3,11 @@ import Votes from './votes';
 import { createUpvote, deleteUpvote } from '../../actions/upvote_actions';
 import { createDownvote, deleteDownvote } from '../../actions/downvote_actions';
 
-const mSTP = (state) => {
+const mSTP = (state, ownProps) => {
   return {
-
+    questionId: ownProps.match.params.questionId,
+    upvotes: state.entities.questions[ownProps.match.params.questionId].upvotes,
+    downvotes: state.entities.questions[ownProps.match.params.questionId].downvotes
   }
 };
 
@@ -13,5 +15,9 @@ const mDTP = (dispatch) => {
   return {
     createUpvote: (questionId) => dispatch(createUpvote(questionId)),
     deleteUpvote: (questionId) => dispatch(deleteUpvote(questionId)),
+    createDownvote: (questionId) => dispatch(createDownvote(questionId)),
+    deleteDownvote: (questionId) => dispatch(deleteDownvote(questionId))
   }
 };
+
+export default connect(mSTP, mDTP)(Votes);
