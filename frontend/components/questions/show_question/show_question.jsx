fef1ea459bfd6cc54b3d056cc15adcbb80d2497e
bcwan <bcwan8@gmail.com>
2020-06-08 23:16:00 -0700

    Test conditions to guarantee user to login to ask questions

--- frontend/components/questions/show_question/show_question.jsx
+++ frontend/components/questions/show_question/show_question.jsx
@@ -60,7 +60,6 @@ class ShowQuestion extends React.Component {
     }
   }
 
-  
 
   changeModalStatus(status) {
     this.setState({ modalIsOpen: status });
@@ -84,7 +83,11 @@ class ShowQuestion extends React.Component {
           <div className="title-ask-button">
             <h3 className="question-title">{question.title}</h3>
               <Button variant="warning"
-                      onClick={() => this.changeModalStatusForAsk(true)}
+                      onClick={() => 
+                        {
+                           !!currentUser ? this.changeModalStatusForAsk(true) : ''
+                        }
+                      }
                       className="ask-question-single"
               >
                 Ask Question
