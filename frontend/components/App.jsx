e4eff692c76bf770dd70216795638679bd5db0ac
bcwan <bcwan8@gmail.com>
2020-06-08 23:44:01 -0700

    Remove comments for AskQuestionContainer

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -8,8 +8,7 @@ import LoginContainer from "./user_session/login_form_container"
 import SignUpContainer from "./user_session/signup_form_container"
 import NavBarContainer from './navbar/navbar_container';
 import SideBarContainer from './sidebar/sidebar_container';
-import AllQuestionsIndexContainer from './questions/all_questions_index/all_questions_index_container'
-// import AskQuestionContainer from './questions/new_question/ask_question_container';
+import AllQuestionsIndexContainer from './questions/all_questions_index/all_questions_index_container';
 import ShowQuestionContainer from './questions/show_question/show_question_container';
 import HomeComponent from './home/home';
 
@@ -21,7 +20,6 @@ const App = () => (
     </header>
     <Switch>
       <Route exact path="/" component={HomeComponent}/>
-      {/* <LoggedInRoute exact path="/questions/ask" component={AskQuestionContainer} /> */}
 
       <Route exact path="/questions/:questionId" component={ShowQuestionContainer} />
       <Route exact path="/questions" component={AllQuestionsIndexContainer}/>
