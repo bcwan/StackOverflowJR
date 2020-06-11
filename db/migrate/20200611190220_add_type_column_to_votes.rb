9ded1d92edaa865f98cb80f282091fe32964e4d1
bcwan <bcwan8@gmail.com>
2020-06-11 14:43:00 -0700

    Make default values into 0 for answer id

--- db/migrate/20200611190220_add_type_column_to_votes.rb
+++ db/migrate/20200611190220_add_type_column_to_votes.rb
@@ -1,6 +1,6 @@
 class AddTypeColumnToVotes < ActiveRecord::Migration[5.2]
   def change
-    add_column :upvotes, :answer_id, :integer
-    add_column :downvotes, :answer_id, :integer
+    add_column :upvotes, :answer_id, :integer, :default => 0
+    add_column :downvotes, :answer_id, :integer, :default => 0
   end
 end
--- test/fixtures/downvotes.yml
+++ test/fixtures/downvotes.yml
@@ -5,7 +5,7 @@
 #  id          :bigint           not null, primary key
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
-#  answer_id   :integer
+#  answer_id   :integer          default(0)
 #  question_id :integer          not null
 #  user_id     :integer          not null
 #
