34143d9b905a36c67fa762f469c9eab14e905e30
bcwan <bcwan8@gmail.com>
2020-06-13 22:51:47 -0700

    Remove window store testing code

--- frontend/stackoverflow_jr.jsx
+++ frontend/stackoverflow_jr.jsx
@@ -2,22 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from './store/store';
 import Root from './components/root';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import { 
-//   fetchQuestion, 
-//   fetchQuestions, 
-//   createQuestion,
-//   updateQuestion,
-//   deleteQuestion
-// } from './actions/questions_actions'
 
-// import {
-//   fetchAnswersForQuestion,
-//   fetchAnswer,
-//   createAnswer,
-//   updateAnswer,
-//   deleteAnswer
-// } from './actions/answers_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
@@ -35,24 +20,6 @@ document.addEventListener("DOMContentLoaded", () => {
     store = configureStore();
   }
 
-  // test out store
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-
-
-  //test question actions
-  // window.fetchQuestion = fetchQuestion;
-  // window.fetchQuestions = fetchQuestions;
-  // window.createQuestion = createQuestion;
-  // window.updateQuestion = updateQuestion;
-  // window.deleteQuestion = deleteQuestion;
-
-  // test answer actions
-  // window.fetchAnswersForQuestion = fetchAnswersForQuestion;
-  // window.fetchAnswer = fetchAnswer;
-  // window.createAnswer = createAnswer;
-  // window.updateAnswer = updateAnswer;
-  // window.deleteAnswer = deleteAnswer;
 
   ReactDOM.render(<Root store={store}/>, root);
 });
