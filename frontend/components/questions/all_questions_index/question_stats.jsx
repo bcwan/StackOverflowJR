69a016d88f3fbf76443388d1235709cf5f790efb
bcwan <bcwan8@gmail.com>
2020-06-15 16:35:55 -0700

    Add styling to question stats for answers

--- frontend/components/questions/all_questions_index/question_stats.jsx
+++ frontend/components/questions/all_questions_index/question_stats.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 
+
 const QuestionStats = ({ question }) => {
-  return (
-    <div className="stats">
-      <div className="stats-votes">
-        <p className="stats-votes-number">{question.upvotes - question.downvotes}</p>
-        <p className="stats-votes-desc">votes</p>
+  let votesCount = question.upvotes - question.downvotes;
+  let answerCount = question.answer_count;
+  if (answerCount > 0) {
+    return (
+      <div className="stats">
+        <div className="stats-votes">
+          <p className="stats-votes-number">{votesCount}</p>
+          <p className="stats-votes-desc">votes</p>
+        </div>
+        <div className="stats-answer-count answers-exist">
+          <p className="stats-answer-number green-text">{answerCount}</p>
+          <p className="stats-answer-desc green-text">answers</p>
+        </div>
       </div>
-      <div className="stats-answer-count">
-        <p className="stats-answer-number">{question.answer_count}</p>
-        <p className="stats-answer-desc">answers</p>
-      </div>
-    </div>
-  );
+    );
+  } else {
+      return (
+        <div className="stats">
+          <div className="stats-votes">
+            <p className="stats-votes-number">{votesCount}</p>
+            <p className="stats-votes-desc">votes</p>
+          </div>
+          <div className="stats-answer-count">
+            <p className="stats-answer-number">{question.answer_count}</p>
+            <p className="stats-answer-desc">answers</p>
+          </div>
+        </div>
+      );
+  }
 }
 
 export default QuestionStats;
