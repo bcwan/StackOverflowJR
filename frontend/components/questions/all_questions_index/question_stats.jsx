33f5b9818fa0b86d758c6f6081eb01ee8f0b554d
bcwan <bcwan8@gmail.com>
2020-06-15 14:26:14 -0700

    Add classes to stats HTML to stylize

--- frontend/components/questions/all_questions_index/question_stats.jsx
+++ frontend/components/questions/all_questions_index/question_stats.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const QuestionStats = ({ question }) => {
   return (
     <div>
-      <div>
-        <p>{question.upvotes - question.downvotes}</p>
-        <p>votes</p>
+      <div className="stats-votes">
+        <p className="stats-votes-count">{question.upvotes - question.downvotes}</p>
+        <p className="stats-votes-desc">votes</p>
       </div>
-      <div>
-        <p>{question.answer_count}</p>
-        <p>answers</p>
+      <div className="stats-answer-count">
+        <p className="stats-answer-count">{question.answer_count}</p>
+        <p className="stats-answer-desc">answers</p>
       </div>
     </div>
   );
