022ef71f3b324366a976358aa0a6d156a08add27
bcwan <bcwan8@gmail.com>
2020-06-15 14:47:02 -0700

    Add styling to answers count in index

--- frontend/components/questions/all_questions_index/question_stats.jsx
+++ frontend/components/questions/all_questions_index/question_stats.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const QuestionStats = ({ question }) => {
   return (
-    <div>
+    <div className="stats">
       <div className="stats-votes">
-        <p className="stats-votes-count">{question.upvotes - question.downvotes}</p>
+        <p className="stats-votes-number">{question.upvotes - question.downvotes}</p>
         <p className="stats-votes-desc">votes</p>
       </div>
       <div className="stats-answer-count">
-        <p className="stats-answer-count">{question.answer_count}</p>
+        <p className="stats-answer-number">{question.answer_count}</p>
         <p className="stats-answer-desc">answers</p>
       </div>
     </div>
