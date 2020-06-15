514bc717c8b944bbf711c74a78d71f48dfd51647
bcwan <bcwan8@gmail.com>
2020-06-14 23:01:43 -0700

    Add additional upvote actions for answers

--- frontend/actions/upvote_actions.js
+++ frontend/actions/upvote_actions.js
@@ -1,6 +1,7 @@
 import * as UpvoteApiUtil from '../util/upvote_api_util';
 
 export const UPDATE_UPVOTE = 'UPDATE_UPVOTE';
+export const UPDATE_ANSWER_UPVOTE = 'UPDATE_ANSWER_UPVOTE';
 
 const updateUpvote = vote => {
   return {
@@ -8,6 +9,13 @@ const updateUpvote = vote => {
     vote
   }
 };
+
+const updateAnswerUpvote = vote => {
+  return {
+    type: UPDATE_ANSWER_UPVOTE,
+    vote
+  }
+}
 
 
 // thunk action creators
@@ -19,10 +27,16 @@ export const createUpvote = (questionId, answerId = 0) => (dispatch) => {
     .then(updatedUpvotes => dispatch(updateUpvote(updatedUpvotes)))
 };
 
+export const createAnswerUpvote = (questionId, answerId) => (dispatch) => {
+  return UpvoteApiUtil.createUpvote(questionId, answerId)
+    .then(updatedUpvotes => dispatch(updateAnswerUpvote(updatedUpvotes)))
+};
+
 // delete will returns an updated upvote number if user decides not to upvote
 // it returns all the question attributes, including the new upvote number ~ its how controllers are set
 export const deleteUpvote = (questionId) => (dispatch) => {
   return UpvoteApiUtil.deleteUpvote(questionId)
     .then(updatedUpvotes => dispatch(updateUpvote(updatedUpvotes)))
 };
+
 
