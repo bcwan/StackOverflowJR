8a1eaea9ccb059c5216f117d9ff88227cd04e111
bcwan <bcwan8@gmail.com>
2020-06-15 00:14:05 -0700

    Found that question voting has bug but answer voting is good

--- app/controllers/api/downvotes_controller.rb
+++ app/controllers/api/downvotes_controller.rb
@@ -4,27 +4,36 @@ class Api::DownvotesController < ApplicationController
 
   def create
     if already_voted?
+      byebug
       render json: ['User has an downvote already'], status: 422
     else
       if has_upvote?
+        byebug
         @upvote = Upvote.find_by(user_id: current_user.id, question_id: params[:question_id], answer_id: params[:downvote][:answer_id])
         @upvote = Upvote.destroy(@upvote.id)
       end
       @downvote = Downvote.new(downvote_params)
+      byebug
       @downvote.user_id = current_user.id
       @downvote.question_id = params[:question_id]
       if @downvote.answer_id != 0
+        byebug
         if @downvote.save!
+          byebug
           @answer = @downvote.answer
           render 'api/answers/show'
         else
+          byebug
           render json: ['Cannot downvote answer'], status: 422
         end
       else
+        byebug
         if @downvote.save
+          byebug
           @question = @downvote.question
           render 'api/questions/show'
         else
+          byebug
           render json: ['Cannot downvote question'], status: 422
         end
       end
--- app/controllers/api/upvotes_controller.rb
+++ app/controllers/api/upvotes_controller.rb
@@ -4,27 +4,35 @@ class Api::UpvotesController < ApplicationController
 
   def create
     if already_voted?
+      byebug
       render json: ['User has an upvote already'], status: 422
     else
       if has_downvote?
+        byebug
         @downvote = Downvote.find_by(user_id: current_user.id, question_id: params[:question_id], answer_id: params[:upvote][:answer_id])
         @downvote = Downvote.delete(@downvote.id)
       end
+      byebug
       @upvote = Upvote.new(upvote_params)
       @upvote.user_id = current_user.id
       @upvote.question_id = params[:question_id]
       if @upvote.answer_id != 0
+        byebug
         if @upvote.save!
+          byebug
           @answer = @upvote.answer
           render 'api/answers/show'
         else
+          byebug
           render json: ['Cannot upvote answer'], status: 422
         end
       else
         if @upvote.save
+          byebug
           @question = @upvote.question
           render 'api/questions/show'
         else
+          byebug
           render json: ['Cannot upvote question'], status: 422
         end
       end
