c7d666ad6c34d295b51b7b26a48928ce75110c56
bcwan <bcwan8@gmail.com>
2020-06-14 17:09:34 -0700

    Test question voting after integrating answer voting

--- app/controllers/api/downvotes_controller.rb
+++ app/controllers/api/downvotes_controller.rb
@@ -4,28 +4,36 @@ class Api::DownvotesController < ApplicationController
 
   def create
     if already_voted?
+      byebug
       render json: ['User has an downvote already'], status: 422
     else
       if has_upvote?
+        byebug
         #@upvote = Upvote.find_by(user_id: current_user.id, question_id: params[:question_id])
-        @upvote = Upvote.find_by(user_id: current_user.id, question_id: params[:question_id], answer_id: params[:answer_id])
+        @upvote = Upvote.find_by(user_id: current_user.id, question_id: params[:question_id], answer_id: params[:downvote][:answer_id])
         @upvote = Upvote.destroy(@upvote.id)
       end
+      byebug
       @downvote = Downvote.new(downvote_params)
       @downvote.user_id = current_user.id
       @downvote.question_id = params[:question_id]
       if @downvote.answer_id != 0
+        byebug
         if @downvote.save
+          byebug
           @answer = @downvote.answer
           render 'api/answers/show'
         else
+          byebug
           render json: ['Cannot downvote answer'], status: 422
         end
       else
         if @downvote.save
+          byebug
           @question = @downvote.question
           render 'api/questions/show'
         else
+          byebug
           render json: ['Cannot downvote question'], status: 422
         end
       end
@@ -40,7 +48,7 @@ class Api::DownvotesController < ApplicationController
   end
 
   def destroy
-    @downvote = Downvote.find_by(user_id: current_user.id, question_id: params[:question_id], answer_id: params[:answer_id])
+    @downvote = Downvote.find_by(user_id: current_user.id, question_id: params[:question_id], answer_id: params[:downvote][:answer_id])
     if @downvote.nil?
       render json: ['Downvote cannot be found or user did not downvote'], status: 422
     else
@@ -56,12 +64,14 @@ class Api::DownvotesController < ApplicationController
   private
 
   def already_voted?
-    Downvote.where(user_id: current_user.id, question_id: params[:question_id], answer_id: params[:answer_id]).exists?
+    byebug
+    Downvote.where(user_id: current_user.id, question_id: params[:question_id], answer_id: params[:downvote][:answer_id]).exists?
   end
 
   # helper method that checks to see there's nothing in Upvotes
   def has_upvote?
-    Upvote.where(user_id: current_user.id, question_id: params[:question_id], answer_id: params[:answer_id]).exists?
+    byebug
+    Upvote.where(user_id: current_user.id, question_id: params[:question_id], answer_id: params[:downvote][:answer_id]).exists?
   end
 
   def downvote_params
