95d438ba502ca098dac53b128586c89c7bbbeca4
bcwan <bcwan8@gmail.com>
2020-06-16 20:36:57 -0700

    Change question props to answer props

--- frontend/components/votes/answer_votes.jsx
+++ frontend/components/votes/answer_votes.jsx
@@ -59,8 +59,8 @@ class AnswerVotes extends React.Component {
 
   indicateVote() {
     let currentUserId = parseInt(this.props.currentUser.id);
-    let userUpvoted = this.props.question.upvotes_user_id.includes(currentUserId);
-    let userDownvoted = this.props.question.downvotes_user_id.includes(currentUserId);
+    let userUpvoted = this.props.answer.upvotes_user_id.includes(currentUserId);
+    let userDownvoted = this.props.answer.downvotes_user_id.includes(currentUserId);
 
     let upvoteArrow = document.getElementById("up-arrow-outline");
     let downvoteArrow = document.getElementById("down-arrow-outline");
