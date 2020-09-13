16cfd5f1dce8aaf95c74844f38651f5ba408e6a5
bcwan <bcwan8@gmail.com>
2020-09-13 15:16:34 -0700

    Set up SearchQuestionsItem component

--- frontend/components/questions/search_question/search_questions_item.jsx
+++ frontend/components/questions/search_question/search_questions_item.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ListGroupItem } from 'react-bootstrap';
-import SearchQuestionStats from './search_question_stats';
+//import SearchQuestionStats from './search_question_stats';
+
+const SearchQuestionsItem = () => {
+  return null;
+}
+
+export default SearchQuestionsItem;
 
