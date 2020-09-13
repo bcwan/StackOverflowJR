a3ddf9af6be8f52bc87c9219ad45e7608bfbfb85
bcwan <bcwan8@gmail.com>
2020-09-12 23:21:38 -0700

    Import components and Bootstrap to search component

--- frontend/components/questions/search_question/search_questions_index.jsx
+++ frontend/components/questions/search_question/search_questions_index.jsx
@@ -1,2 +1,9 @@
 import React from 'react';
 import SearchQuestionsItem from './search_questions_item';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import SideBarContainer from '../../sidebar/sidebar';
+import AskQuestion from '../new_question/ask_question';
+import Modal from 'react-modal';
+
+
