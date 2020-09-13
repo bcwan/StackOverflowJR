c749c02d62ae7e7665fdb9f954768b1057871263
bcwan <bcwan8@gmail.com>
2020-09-12 21:19:58 -0700

    Add logger for debugging purposes

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
-//import logger from "redux-logger";
+import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const configureStore = (preloadedState = {}) => (
-  //createStore(rootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk, logger)))
-  createStore(rootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk)))
+  createStore(rootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk, logger)))
+  //createStore(rootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk)))
 );
 
 export default configureStore;
