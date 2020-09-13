e72064d83a1362f4c3781883bd510f5ae2c5dcf5
bcwan <bcwan8@gmail.com>
2020-09-13 15:40:36 -0700

    Import thunk action calls for SearchQuestionsContainer

---
+++ frontend/components/questions/search_question/search_questions_container.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import SearchQuestionsIndex from './search_questions_index';
+import {
+  fetchQuestions,
+  fetchQuestion,
+  createQuestion
+} from '../../../actions/questions_actions';
+
