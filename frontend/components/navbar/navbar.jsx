5db3a9ab29895e0073f7a00f2e9d2c54b4c538d5
bcwan <bcwan8@gmail.com>
2020-09-13 17:15:13 -0700

    Uncomment search bar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -30,7 +30,7 @@ const NavBar = ( { currentUser, logout } ) => {
       <Link to="/"><img className="stackoverflow-logo" src="/assets/stackoverflowjr.png" alt="" /></Link>
       <nav className="header-nav">
      
-        {/*<input className="search-bar" type="text" placeholder=" Search..." />*/}
+        <input className="search-bar" type="text" placeholder=" Search..." />
         {
           user ? loggedInHTML : notLoggedInHTML
         }
