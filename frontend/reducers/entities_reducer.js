6a09856745c540ae0ffeb54980f1f2d5e6325ebd
bcwan <bcwan8@gmail.com>
2020-09-21 23:54:02 -0700

    Add search questions reducer to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import QuestionReducer from './questions_reducer';
 import AnswerReducer from './answers_reducer';
+import SearchQuestionReducer from './search_questions_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   questions: QuestionReducer,
-  answers: AnswerReducer
+  answers: AnswerReducer,
+  search_questions: SearchQuestionReducer
 });
 
 export default entitiesReducer;
