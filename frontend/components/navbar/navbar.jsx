accff9acf83fe16a8bab3e680b912d864ba869ed
bcwan <bcwan8@gmail.com>
2020-09-26 18:57:56 -0700

    Fix login issue

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -3,63 +3,71 @@ import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import { withRouter } from "react-router-dom";
 
-const NavBar = ({ history }, { currentUser, logout } ) => {
-
-  const redirectToSearchPage = () => {
-    let input = document.getElementById("search-bar-text").value;
-    let parse = input.split(" ").join("+")
-    return history.push(`/search?search=${parse}`);
-  };
+// const NavBar = ({ history }, { currentUser, logout } ) => {
+const NavBar = ({ currentUser, logout }) => {
+  // const redirectToSearchPage = () => {
+  //   let input = document.getElementById("search-bar-text").value;
+  //   let parse = input.split(" ").join("+")
+  //   return history.push(`/search?search=${parse}`);
+  // };
 
   //profile button and logout button
-  const loggedInButtons = () => (
-    <div className="auth-btn-set">
-      <Button variant="success">
-        {currentUser ? currentUser.username : "Profile"}
-      </Button>
-      <Link to="/">
-        <Button variant="dark" onClick={logout}>
-          Log out
+  const loggedInButtons = () => {
+    return (
+      <div className="auth-btn-set">
+        <Button variant="success">
+          {currentUser ? currentUser.username : "Profile"}
         </Button>
-      </Link>
-    </div>
-  );
+        <Link to="/">
+          <Button variant="dark" onClick={logout}>
+            Log out
+          </Button>
+        </Link>
+      </div>
+    );
+  };
 
-  const notLoggedInButtons = () => (
-    <div className="auth-btn-set">
-      <Link to="/login">
-        <Button variant="success">Log in</Button>
-      </Link>
-      <Link to="/signup">
-        <Button variant="primary">Sign up</Button>
-      </Link>
-    </div>
-  );
+  const notLoggedInButtons = () => {
+    return (
+      <div className="auth-btn-set">
+        <Link to="/login">
+          <Button variant="success">Log in</Button>
+        </Link>
+        <Link to="/signup">
+          <Button variant="primary">Sign up</Button>
+        </Link>
+      </div>
+    );
+  };
 
-  const navBarDesign = (user, loggedInHTML, notLoggedInHTML) => (
-    <header className="header">
-      <Link to="/">
-        <img
-          className="stackoverflow-logo"
-          src="/assets/stackoverflowjr.png"
-          alt=""
-        />
-      </Link>
-      <nav className="header-nav">
-        <form onSubmit={redirectToSearchPage}>
-          <input
-            className="search-bar"
-            id ="search-bar-text"
-            type="text"
-            placeholder=" Search...press enter"
+  const navBarDesign = (user, loggedInHTML, notLoggedInHTML) => {
+    return (
+      <header className="header">
+        <Link to="/">
+          <img
+            className="stackoverflow-logo"
+            src="/assets/stackoverflowjr.png"
+            alt=""
           />
+        </Link>
+        <nav className="header-nav">
+          {/* <form onSubmit={redirectToSearchPage}> */}
+          <form>
+            <input
+              className="search-bar"
+              id="search-bar-text"
+              type="text"
+              placeholder=" Search...press enter"
+            />
+          </form>
           {user ? loggedInHTML : notLoggedInHTML}
-        </form>
-      </nav>
-    </header>
-  );
+        </nav>
+      </header>
+    );
+  };
 
   return navBarDesign(currentUser, loggedInButtons(), notLoggedInButtons());
 };
 
-export default withRouter(NavBar);
+export default NavBar;
+//export default withRouter(NavBar);
