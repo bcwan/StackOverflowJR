4d91baa2631bcaed091058b2782dfabe0be1d88d
bcwan <bcwan8@gmail.com>
2022-08-10 17:26:26 -0700

    Remove comments

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,7 +3,6 @@ import { Provider } from "react-redux";
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 
-// Here's the Root of Application
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
